5bd34d8f2eb0bf5482ecb9707e8935f09a27224a
Gabriel D <gabriel.drutu@gmail.com>
2020-03-20 16:53:10 +0200

    Sign in form components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 
 const HatsPage = () => (
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
