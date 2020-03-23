ee9719c118687b63509884b15621726925a0aa3a
Gabriel D <gabriel.drutu@gmail.com>
2020-03-23 16:43:14 +0200

    Signout and menu signin

--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -5,7 +5,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-out.styles.scss';
 
 const SignInAndSignUpPage = () => (
-  <div class='sign-in-and-sign-up'>
+  <div className='sign-in-and-sign-up'>
     <SignIn />
   </div>
 );
