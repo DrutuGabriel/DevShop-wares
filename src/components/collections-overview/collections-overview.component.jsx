14f66a8e3ea5219454b110097f82c71e184512c6
Gabriel D <gabriel.drutu@gmail.com>
2020-04-21 19:20:59 +0300

    Create selector for collections as list

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,13 +4,13 @@ import {createStructuredSelector} from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({collections}) => ( 
   <div className='collections-overview'>
-    {Object.values(collections).map(({id, ...otherCollectionProps}) => (
+    {collections.map(({id, ...otherCollectionProps}) => (
       <CollectionPreview 
         key={id}
         id={id}  
@@ -21,7 +21,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
  
 export default connect(mapStateToProps)(CollectionsOverview);
