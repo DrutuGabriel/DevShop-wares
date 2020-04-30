b53aa9712db43342ca216a3a8ae9b83d7b892c15
Gabriel D <gabriel.drutu@gmail.com>
2020-04-30 10:29:18 +0300

    Refactor collection-overview to use styled-components

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,10 +6,12 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 
 import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
+import {
+  CollectionsOverviewContainer 
+} from './collections-overview.styles';
 
 const CollectionsOverview = ({collections}) => ( 
-  <div className='collections-overview'>
+  <CollectionsOverviewContainer>
     {collections.map(({id, ...otherCollectionProps}) => (
       <CollectionPreview 
         key={id}
@@ -17,7 +19,7 @@ const CollectionsOverview = ({collections}) => (
         {...otherCollectionProps} 
       />
     ))}
-  </div> 
+  </CollectionsOverviewContainer> 
 );
 
 const mapStateToProps = createStructuredSelector({
