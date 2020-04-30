3995ed45e994b778f709d35a00fda7bd2b7bf573
Gabriel D <gabriel.drutu@gmail.com>
2020-04-30 11:25:56 +0300

    Refactor menu-item tu use styled-components

---
+++ src/components/menu-item/menu-item.styles.jsx
@@ -0,0 +1,84 @@
+import styled, {css} from 'styled-components';
+
+
+const largeMenuItem = css`
+  height: 380px;
+`;
+
+const getMenuItemStyles = props => {
+  if(props.size == 'large'){
+    return largeMenuItem;
+  }
+
+  return '';
+};
+
+export const MenuItemContainer = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  position: relative;
+  overflow: hidden;
+
+  &:first-child {
+    margin-right: 7.5px;
+  }
+
+  &:last-child {
+    margin-left: 7.5px;
+  }
+
+  ${getMenuItemStyles}
+
+  &:hover {
+    cursor: pointer;
+
+    & .background-image {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+
+    & .content {
+      opacity: 0.9;
+    }
+  }
+`;
+
+export const BackgroundImageContainer = styled.div`
+  background-position: center;
+  background-size: cover;
+  background-image: ${({imageUrl}) => `url(${imageUrl})`};
+  width: 100%;
+  height: 100%;
+  transition: transform 2s cubic-bezier(0.25, 0.45, 0.45, 0.95);  
+`;
+
+export const ContentContainer = styled.div`
+  height: 90px;
+  padding: 0 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  background-color: #fff;
+  opacity: 0.7;
+  position: absolute;
+`;
+
+export const ContentTitle = styled.span`
+  font-weight: bold;
+  margin-bottom: 6px;
+  font-size: 22px;
+  color: #4a4a4a;
+`;
+
+export const ContentSubtitle = styled.span`
+  font-weight: lighter;
+  font-size: 16px;
+`;
