7cf4a36b5067c189396fb40146f4695c7c8b209c
Gabriel D <gabriel.drutu@gmail.com>
2020-05-02 00:16:01 +0300

    Refactor checkout page to use styled-components

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,28 +7,34 @@ import StripeCheckoutButton from '../../components/stripe-button/stripe-button.c
 
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 
+import {
+  CheckoutPageContainer,
+  CheckoutHeaderContainer,
+  HeaderBlockContainer,
+  TotalContainer,
+  WarningContainer
+} from './checkout.styles';
 
-import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems, total}) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
+  <CheckoutPageContainer>
+    <CheckoutHeaderContainer>
+      <HeaderBlockContainer>
         <span>Product</span>
-      </div>
-      <div className='header-block'>
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Description</span>
-      </div>
-      <div className='header-block'>
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Quantity</span>
-      </div>
-      <div className='header-block'>
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Price</span>
-      </div>
-      <div className='header-block'>
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Remove</span>
-      </div>
-    </div>
+      </HeaderBlockContainer>
+    </CheckoutHeaderContainer>
     {cartItems.length ? (
       cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
@@ -36,16 +42,16 @@ const CheckoutPage = ({cartItems, total}) => (
         <span>No items</span>
       )
     }
-    <div className='total'>
+    <TotalContainer>
       <span>TOTAL: ${total}</span>
-    </div>
-    <div className='test-warning'>
+    </TotalContainer>
+    <WarningContainer>
       * Please use the following test credit card for payments* 
       <br />
       4242 4242 4242 4242 - Exp: 12/23 - CVV: 123
-    </div>
+    </WarningContainer>
     <StripeCheckoutButton price={total} />
-  </div>
+  </CheckoutPageContainer>
 );
 
 
