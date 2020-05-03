79037fe06e95fff7585325338708341efbc616a8
Gabriel D <gabriel.drutu@gmail.com>
2020-05-03 14:32:45 +0300

    Shop page refactor to class

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {
+  auth, 
+  createUserProfileDocument
+} from './firebase/firebase.utils';
 
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
+
+import {selectCollectionsForPreview} from './redux/shop/shop.selectors';
 
 
 class App extends React.Component {
@@ -36,8 +41,7 @@ class App extends React.Component {
         });
       } else {
         setCurrentUser(userAuth);
-      }
-    
+      }    
     });
   }
 
