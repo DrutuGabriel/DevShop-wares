45b3c096398d3b0c6fbe775cfdac85ebe211a621
Gabriel D <gabriel.drutu@gmail.com>
2020-07-17 15:38:49 +0300

    useEffect in our app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
@@ -8,28 +8,25 @@ import CollectionsOverviewContainer from '../../components/collections-overview/
 import CollectionPageContainer from '../collection/collection.container';
 
 
-class ShopPage extends React.Component {
-  componentDidMount(){   
-    const {fetchCollectionsStart} = this.props;
-    fetchCollectionsStart();
-  }
+const ShopPage = ({fetchCollectionsStart, match}) => {
 
-  render(){
-    const {match} = this.props;
+  useEffect(() => {
+    fetchCollectionsStart()
+  }, [fetchCollectionsStart]);
 
-    return (
-      <div className='shop-page'>
-        <Route 
-          exact 
-          path={`${match.path}`} 
-          component={CollectionsOverviewContainer}
-        />
-        <Route 
-          path={`${match.path}/:collectionId`} 
-          component={CollectionPageContainer}
-        />
-      </div> 
-  );}
+  return (
+    <div className='shop-page'>
+      <Route 
+        exact 
+        path={`${match.path}`} 
+        component={CollectionsOverviewContainer}
+      />
+      <Route 
+        path={`${match.path}/:collectionId`} 
+        component={CollectionPageContainer}
+      />
+    </div> 
+  );
 }
 
 
