769c6d1a0434357efc155394b98020c80563fbbe
Gabriel D <gabriel.drutu@gmail.com>
2020-08-19 23:18:01 +0300

    Prepare for PWA

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compresssion = require('compression');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
@@ -16,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(compresssion());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production'){
@@ -31,6 +32,12 @@ if(process.env.NODE_ENV === 'production'){
 app.listen(port, error => {
   if(error){throw error} 
   console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(
+    __dirname, '..', 'build', 'service-worker.js'
+  ));
 });
 
 app.post('/payment', (req, res) => {
